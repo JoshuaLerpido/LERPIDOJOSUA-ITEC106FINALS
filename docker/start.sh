131afec2310fb21e6c9fab9f1746096ae48fe11c
JoshuaLerpido <joshualerpido21@gmail.com>
2026-05-28 16:30:33 +0800

    add docker nginx and start script

--- docker/start.sh
+++ docker/start.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
-
-# Generate app key if not set
-php artisan key:generate --force
-
-# Run migrations
-php artisan migrate --force
-
-# Cache for performance
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Storage link
-php artisan storage:link || true
-
-# Start PHP-FPM in background
+php /var/www/html/artisan config:cache
+php /var/www/html/artisan route:cache
+php /var/www/html/artisan view:cache
+php /var/www/html/artisan storage:link
+php /var/www/html/artisan migrate --force
 php-fpm -D
-
-# Start Nginx in foreground
 nginx -g "daemon off;"
